377bd8487ec6b4ece6056601c57dcd6b20e9ead9
Surya <surya334r@gmail.com>
2026-04-05 22:48:20 +0530

    Update db.py

--- db.py
+++ db.py
@@ -6,11 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-
-engine = create_engine(DATABASE_URL, echo=True)
-
-print(DATABASE_URL)
-engine = create_engine(DATABASE_URL, echo=True,pool_pre_ping=True)
+(DATABASE_URL, echo=True,pool_pre_ping=True)
 
 
 SessionLocal = sessionmaker(bind=engine)
